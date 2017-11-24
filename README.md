0190ff3c199af0d6dd52d3f5e8d522d9566007ac
Kirill Maslov <maslovks@gmail.com>
2017-11-24 20:02:47 +0300

    use md5 hash sum

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This script lists all duplicate files in a given directory.
 
 The script ouputs the list in the following format:
 <file_name> (<file_size> bytes) at: <file_directory_path>
+    <hash_sum_md5>
 
 # How to use
 To use this script, one should use python3.5.
@@ -14,10 +15,17 @@ The script takes one positional argument:
 This example shows how to find all duplicates in 'UserName/problem11' directory
 ```
 $ python duplicates.py 'UserName/problem11'
-.DS_Store (6148 bytes): at UserName/problem11
-.DS_Store (6148 bytes): at UserName/problem11/11_duplicates
-master (41 bytes): at UserName/problem11/11_duplicates/.git/refs/heads
-master (41 bytes): at UserName/problem11/11_duplicates/.git/refs/remotes/origin
+master (41 bytes): at /Users/KirillMaslov/Documents/Projects/devman/problem11/11_duplicates/.git/refs/heads
+	0459474b7aaae584aca70c3884db91a8
+
+master (41 bytes): at /Users/KirillMaslov/Documents/Projects/devman/problem11/11_duplicates/.git/refs/remotes/origin
+	0459474b7aaae584aca70c3884db91a8
+
+HEAD (990 bytes): at /Users/KirillMaslov/Documents/Projects/devman/problem11/11_duplicates/.git/logs
+	d950d0646a7ef5b046125ef17fd53e79
+
+master (990 bytes): at /Users/KirillMaslov/Documents/Projects/devman/problem11/11_duplicates/.git/logs/refs/heads
+	d950d0646a7ef5b046125ef17fd53e79
 ```
 
 # Project Goals
--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,8 @@
 import os
 from collections import Counter
 import argparse
+import hashlib
+
 
 class File:
 
@@ -10,17 +12,32 @@ class File:
         self.file_path = os.path.join(dir_path, file_name)
         self.size = os.path.getsize(self.file_path)
 
-        self._hash_value = hash((self.file_name, self.size))
+        self.hash_sum = self._calc_hash_sum()
+
+    def _calc_hash_sum(self):
+        with open(self.file_path, 'rb') as file:
+            hash_obj = hashlib.md5()
+            for chunk in self._chunk_reader(file):
+                hash_obj.update(chunk)
+            return hash_obj.hexdigest()
+
+    def _chunk_reader(self, file, chunk_size=1024):
+        while True:
+            chunk = file.read(chunk_size)
+            if not chunk:
+                return
+            yield chunk
 
     def __eq__(self, other):
         return self.file_name, self.size == other.file_name, other.size
 
     def __hash__(self):
-        return self._hash_value
+        return int(self.hash_sum, 16)
 
     def __str__(self):
-        return "{file_name} ({size} bytes): at {path}".format(
-            file_name=self.file_name, path=self.dir_path, size=self.size)
+        return "{file_name} ({size} bytes): at {path}\n\t{hash}\n".format(
+            file_name=self.file_name, path=self.dir_path, size=self.size,
+            hash=self.hash_sum)
 
     def __repr__(self):
         return "File({}, {})".format(self.dir_path, self.file_name)
@@ -47,7 +64,7 @@ def find_duplicates(files_list):
     files_counter = Counter(files_list)
     duplicates = [file for file in files_list
                   if files_counter[file] > 1]
-    duplicates.sort(key=lambda file: (file.file_name, file.size))
+    duplicates.sort(key=lambda file: (file.hash_sum, file.file_name))
     return duplicates
 
 
